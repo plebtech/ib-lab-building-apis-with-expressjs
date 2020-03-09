78d014bc84e7e2c7ea16164f69da46e83f95dbf2
Nick Mc <nbmc@protonmail.com>
2020-03-09 09:34:28 -0500

    modified server to get index.html from client directory.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const apiRouter = require('./routes');
@@ -6,6 +7,10 @@ const serv = express();
 
 serv.use(cors());
 serv.use(express.json());
+
+serv.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 serv.use('/api', apiRouter);
 
