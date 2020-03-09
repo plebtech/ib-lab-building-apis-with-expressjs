a319acf87bc5fcf67bae8ecf54b2527669bcbebf
Nick Mc <nbmc@protonmail.com>
2020-03-09 12:49:54 -0500

    implemented storage of new chirps via UI.

--- client/app.js
+++ client/app.js
@@ -5,19 +5,43 @@ $(document).ready(function () {
     });
 });
 
+let chirps;
+const chirpsGet = () => {
+    $.ajax({
+        url: "http://localhost:3000/api/chirps",
+        success: res => {
+            chirps = res;
+            // chirpsPrint(chirps);
+        }
+    });
+}
+
+
+
 $('body').append('<div id="controls"></div>');
 $('#controls').append('<h1>chirpr</h1>');
 
 const chirpModal = () => {
     $('#controls').append('<div id = "chirpModal"></div>');
-    $('#chirpModal').append('<input type="text" name="account" placeholder="account" />');
-    $('#chirpModal').append('<input type="text" name="chirp" placeholder="chirp" />');
+    $('#chirpModal').append('<input id="account" type="text" name="account" placeholder="account" />');
+    $('#chirpModal').append('<input id="content" type="text" name="chirp" placeholder="chirp" />');
     $('#chirpModal').append('<button id="chirp">chirp it</button>');
-    $('#chirp').click(function () {
+    $('#chirp').click(() => {
+        let chirp = {};
+        chirp.account = $('#account').val();
+        chirp.content = $('#content').val();
+        let chirpStr = JSON.stringify(chirp);
+        $.ajax({
+            method: 'POST',
+            url: 'http://localhost:3000/api/chirps',
+            contentType: 'application/json; charset=utf-8',
+            data: chirpStr
+        });
         $('#output').append('<h3>chirp goes here.</h3>');
-    })
+    });
 }
 
 $('body').append('<div id="output"></div>');
 
+chirpsGet();
 chirpModal();
